704985ab69d24eb8f5f69c1db25508dae4821519
Jesús Cabrera <jccsvq@gmail.com>
2021-05-04 18:07:24 +0200

    Arbitrary number of rods
    Also some cosmetic.

--- txtabacus.py
+++ txtabacus.py
@@ -9,20 +9,17 @@ Created on Sat Mar 23 17:57:02 2019
 
 Modified on Sun May 2 2021
 
-:author: jccsvq
+:author: jccsvq.github.io
 
 """
 
-# Data
-
-nr = 13
 
 
 
-def leftpad(str1, n=nr):
+def leftpad(str1, n):
     """
-    leftpad: Returns a list of rod codes of length `n` padding `str1` to the
-    left
+    leftpad: Returns a list of rod codes of length `n` padding `str1` to
+    the left
 
     :param str1: String of codes to pad to the left
     :type str1: str
@@ -30,9 +27,6 @@ def leftpad(str1, n=nr):
     :type n: int
     :returns: List of codes
     :rtype: list
-
-    .. DANGER:: This implementation only works with `n = 13`.
-        Don't change the default!
 
     :example:
 
@@ -46,13 +40,13 @@ def leftpad(str1, n=nr):
     ali = [int(i) for i in str1.split()]
     l0 = len(ali)
     if l0 <= n:
-        ali.extend([0 for i in range(nr - l0)])
+        ali.extend([0 for i in range(n - l0)])
     else:
         ali = ali[:nr-l0]
     return ali
 
 
-def rightpad(str1, n=nr):
+def rightpad(str1, n):
     """
     Returns a list of rod codes of length `n`
     padding ``str1`` to the right
@@ -64,9 +58,6 @@ def rightpad(str1, n=nr):
     :returns: List of codes
     :rtype: list
 
-    .. note::
-           This implementation only works with `n = 13`.
-           Don't change the default!
 
     :example:
 
@@ -80,7 +71,7 @@ def rightpad(str1, n=nr):
     ali = [int(i) for i in str1.split()]
     l0 = len(ali)
     if l0 <= n:
-        ali = [0 for i in range(nr - l0)] + ali
+        ali = [0 for i in range(n - l0)] + ali
     else:
         ali = ali[-nr:]
     return ali
@@ -96,7 +87,8 @@ def tenchi_split(list1):
     :param str1: string with codes for each rod
     :type str1: str
     :returns: tuple containing lists of codes for *Ten* and
-        *Chi* beads in each rod and a string ``name`` with filename for swg.
+        *Chi* beads in each rod and a string ``name`` with filename for 
+        swg.
     :rtype: tuple
 
     :example:
@@ -126,7 +118,7 @@ def tenchi_split(list1):
 
 
 
-def sw_print(str1, left=True, atype='2:5'):
+def sw_print(str1, nr=13, left=True, atype='2:5'):
     """
     Draws the abacus as ascii art
 
@@ -134,6 +126,8 @@ def sw_print(str1, left=True, atype='2:5'):
 
     :param str1: list of raw codes
     :type str1: str
+    :param nr: number of columns
+    :type nr: int
     :param left: ``True`` if leftpad is required, ``False`` for rightpad
     :type left: bool
     :param atype: '1:4', '1:5', '2:5', '3:5'
@@ -162,11 +156,9 @@ def sw_print(str1, left=True, atype='2:5'):
 
     """
 
-    l1 = '   A  B  C  D  E  F  G  H  I  J  K  L  M  '
+    l1 = '   A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T\
+  U  V  W  X  Y  Z'
     l0 = '║ ●'
-    le1 = '╔═════════════════════════════════════════╗'
-    le2 = '╠═════════════════════════════════════════╣'
-    le3 = '╚═════════════════════════════════════════╝'
 
     if atype == '1:4':
         tcod = ['○│', '│●']
@@ -175,54 +167,71 @@ def sw_print(str1, left=True, atype='2:5'):
         nlr = 5
     elif atype == '3:5' :
         tcod = ['○○○│', '○○│●', '○│●●', '│●●●']
-        ccod = ['│○○○○○', '●│○○○○', '●●│○○○', '●●●│○○', '●●●●│○', '●●●●●│']
+        ccod = ['│○○○○○', '●│○○○○', '●●│○○○', '●●●│○○', '●●●●│○', 
+         '●●●●●│']
         nur = 4
         nlr = 6
     elif atype == '1:5' :
         tcod = ['○│', '│●']
-        ccod = ['│○○○○○', '●│○○○○', '●●│○○○', '●●●│○○', '●●●●│○', '●●●●●│']
+        ccod = ['│○○○○○', '●│○○○○', '●●│○○○', '●●●│○○', '●●●●│○',
+         '●●●●●│']
         nur = 2
         nlr = 6
     elif atype == '1:5s' :
         tcod = ['○│', '│●']
-        ccod = ['││○○○○○', '●││○○○○', '●●││○○○', '●●●││○○', '●●●●││○', '●●●●●││', '●│●│○○○', '●●│●│○○', '●●●│●│○', '●●●●│●│']
+        ccod = ['││○○○○○', '●││○○○○', '●●││○○○', '●●●││○○', '●●●●││○',
+         '●●●●●││', '●│●│○○○', '●●│●│○○', '●●●│●│○', '●●●●│●│']
         nur = 2
         nlr = 7
    
     else :
         tcod = ['○○││', '○││●', '││●●', '│●│●']
-        ccod = ['││○○○○○', '●││○○○○', '●●││○○○', '●●●││○○', '●●●●││○', '●●●●●││']
+        ccod = ['││○○○○○', '●││○○○○', '●●││○○○', '●●●││○○', '●●●●││○',
+         '●●●●●││']
         nur = 4
         nlr = 7
     if left:
-        ten, chi, name = tenchi_split(leftpad(str1))
+        ten, chi, name = tenchi_split(leftpad(str1, nr))
     else:
-        ten, chi, name = tenchi_split(rightpad(str1))
+        ten, chi, name = tenchi_split(rightpad(str1, nr))
 
     values = ' '
     diglist = [0,1,2,3,4,5,1,2,3,4]
     for i in range(nr):
         values = values + '{:3d}'.format(5 * ten[i] + diglist[chi[i]])
     print(values)
-    print(le1)
+    line = '╔═'
+    for i in range(nr):
+        line = line + '═══'
+    line = line + '═╗'
+    print(line)
     for i in range(nur):
         line = l0[0:2] + ' '
         for j in range(nr):
             line = line + tcod[ten[j]][i] + '  '
         line = line + l0[0]
         print(line)
-    print(le2)
+    line = '╠═'
+    for i in range(nr):
+        line = line + '═══'
+    line = line + '═╣'
+    print(line)
     for i in range(nlr):
         line = l0[0:2] + ' '
         for j in range(nr):
             line = line + ccod[chi[j]][i] + '  '
         line = line + l0[0]
         print(line)
-    print(le3)
-    print(l1)
+    line = '╚═'
+    for i in range(nr):
+        line = line + '═══'
+    line = line + '═╝'
+    print(line)
+    print(l1[0:3*nr+1])
 
 
 if __name__ == "__main__":
+
     """
     Tests
     """
@@ -230,8 +239,9 @@ if __name__ == "__main__":
     sw_print('0 1 2 3 4 5 10 11 12 13 14 15 20')
     sw_print('21 22 23 24 25 30 31 32 33 34 35')
     sw_print('21 22 23 24 25 30 31 32 33 34 35', atype = '3:5')
-    sw_print('1 2 3', False)
-    sw_print('0 1 2 3 4 10 11 12 13 14 ', atype = '1:4')
+    sw_print('1 2 3', left = False)
+    sw_print('1 2 3', nr=16, left = False)
+    sw_print('1 2 3 4 10 11 12 13 14 ', nr=9, atype = '1:4')
     sw_print('0 1 2 3 4 5 10 11 12 13 14 15', atype = '1:5')
-    sw_print('0 6 7 8 9 5 16 17 18 19 14 15', atype = '1:5s')
+    sw_print('0 6 7 8 9 5 16 17 18 19 14 15', nr=17, atype = '1:5s')
 
